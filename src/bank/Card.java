4db5434e59f2d4b897921afa3196c702b2dce163
Jia Hao To <163077264+Deffyh@users.noreply.github.com>
2025-11-14 15:20:41 -0500

    Update Card.java
    gave it a creditLimit and creditUsage attributes

--- src/bank/Card.java
+++ src/bank/Card.java
@@ -3,8 +3,36 @@ package bank;
 import auth.core.Customer;
 
 public class Card extends Account {
-    public Card(Customer customer) {
+
+    private double creditLimit; //Maximum
+
+    private double creditUsage; //How much of maximum was spent
+
+    public Card(Customer customer, double creditLimit) {
         super(customer);
+
+        if (creditLimit < 0) {
+            throw new IllegalArgumentException("Credit limit cannot be negative");
+        }
+
+        this.creditLimit = creditLimit;
+
+        this.creditUsage = 0;
+    }
+
+    public void setCreditLimit(double newCreditLimit){
+        if (newCreditLimit < 0) {
+            throw new IllegalArgumentException("Credit limit cannot be negative");
+        }
+        this.creditLimit = newCreditLimit;
+    }
+
+    public double getCreditLimit(){
+        return this.creditLimit;
+    }
+
+    public double getCreditUsage() {
+        return creditUsage;
     }
 }
 
